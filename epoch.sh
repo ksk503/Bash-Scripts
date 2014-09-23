1c98edaab0508a95cd873274be4bdfb1f132d2ab
ksk503 <ksk415@gmail.com>
2014-09-23 14:40:27 -0700

    Create epoch.sh

---
+++ epoch.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Basic script to get epoch times
+
+# Test to ensure at least one argument
+if [[ $# -lt 1 ]] ;
+  then
+    echo
+    echo "Usage: epoch.sh start-date end-date[optional"
+    echo "Ex: epoch.sh '2014-02-01 12:00' '2014-02-28 23:59:59'"
+    echo
+    exit 0
+fi
+
+# Set sd (start date) and ed (end date) variables
+sd=$1
+ed=$2
+
+# Do the needful
+function epoch {
+  start=$(date -d "$sd" +%s)
+  end=$(date -d "$ed" +%s)
+}
+
+# Run function and print out results
+epoch
+echo "Start Date: $start"
+
+if [[ -z "$ed" ]] ;
+  then
+    exit 0
+  else
+    echo "End Date:   $end"
+fi
+
+exit 0
