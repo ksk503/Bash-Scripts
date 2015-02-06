119a31d29724e0050d606c6492da6b06f23e4249
ksk503 <ksk415@gmail.com>
2015-02-06 12:52:09 -0800

    Update kfc.sh

--- kfc.sh
+++ kfc.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
+#Define files to check
 declare -a farray=('/etc/passwd' '/etc/resolv.conf' 'test.txt')
-filetime=`stat -c "%Y" $1`
-currtime=`date +%s`
 
-timediff=$(( (currtime - filetime) / 86440 ))
-echo $timediff
+#Uncomment echo for testing
+#echo ${farray[@]}
 
-echo ${farray[@]}
 for F in ${farray[@]}; do
-  ftime=`stat -c "%Y" $F`
-  ctime=`date +%s`
-  dtime=$(( ($ctime - $ftime) / 86440))
-  echo $dtime
+  #get mtime of file
+  ftime=`stat -c "%Y" $F` 
+  #current time
+  ctime=`date +%s` 
+  #Calculate difference
+  dtime=$(( ($ctime - $ftime) / 86440))  
+  #Uncomment echo for testing
+  #echo $F $dtime
+  #Test if time diff is less than 2 days
+  if [ $dtime -lt 2 ]; then
+    #echo "$F changed $dtime days ago."
+    #User logger to write to syslog
+    /usr/bin/logger -t TEST "$F changed $dtime days ago."
+  fi
 done
+
+exit 0
